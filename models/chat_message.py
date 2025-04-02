7b6c07419c44942fa1429a45fe6869c49ee8c924
Subin Khang <subinkhang102@gmail.com>
2025-04-02 14:55:24 +0700

    Update

--- models/chat_message.py
+++ models/chat_message.py
@@ -4,16 +4,13 @@ class ChatMessage(models.Model):
     _name = 'chatopia.message'
     _description = 'Chat Message'
 
-    # ID do Odoo tự cấp (không cần khai báo)
-    chatwoot_message_id = fields.Integer(string="Chatwoot Message ID")  # ID từ Chatwoot
-    inbox_id = fields.Integer(string="Inbox ID")  # ID inbox từ Chatwoot
-    chatwoot_conversation_id = fields.Integer(string="Chatwoot Conversation ID")  # ID conversation từ Chatwoot để nối
-    sender = fields.Char(string="Sender")  # Tên sender, tương tự conversation
-    content = fields.Text(string="Content")  # Nội dung tin nhắn
-    conversation_id = fields.Many2one('chatopia.conversation', string="Conversation")  # Liên kết với conversation trong Odoo
+    chatwoot_message_id = fields.Integer(string="Chatwoot Message ID")
+    inbox_id = fields.Integer(string="Inbox ID")
+    chatwoot_conversation_id = fields.Integer(string="Chatwoot Conversation ID")
+    sender = fields.Char(string="Sender")
+    content = fields.Text(string="Content")
+    conversation_id = fields.Many2one('chatopia.conversation', string="Conversation")
     
     message_type = fields.Selection([('user', 'User'), ('admin', 'Admin')], string="Message Type")
     created_at = fields.Datetime(string="Created At")
     is_read = fields.Boolean(string="Is Read", default=False)
-    
-    
